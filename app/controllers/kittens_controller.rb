10315626f82b0dd79ddbd830f5894f4ec4cc8123
Matt Rangel <matt333three@aol.com>
2023-12-04 15:39:19 -0600

    Create view and actions for creating kittens

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -21,6 +21,19 @@ class KittensController < ApplicationController
     end
   end
 
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.create(kitten_params)
+    if @kitten.save
+      redirect_to @kitten
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
   def kitten_params
     params.require(:kitten).permit(:name, :age, :cuteness, :softness)
