ee08f4091b863bd22041a49d14a3fbf562da7ec0
Matt Rangel <matt333three@aol.com>
2023-12-04 15:29:50 -0600

    Create view and actions for editing kittens

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -6,4 +6,23 @@ class KittensController < ApplicationController
   def show
     @kitten = Kitten.find(params[:id])
   end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+    @kitten.attributes = kitten_params
+    if @kitten.save
+      redirect_to @kitten
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
 end
